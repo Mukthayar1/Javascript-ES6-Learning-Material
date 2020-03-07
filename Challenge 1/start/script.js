74dff8bb3ad421712bcf0c58dea533de1fb6b3cf
hammad-siraj <hammadsiraj224@gmail.com>
2020-03-07 16:37:04 +0500

    challenges

---
+++ Challenge 1/start/script.js
@@ -0,0 +1,24 @@
+/*
+Create a function sum() that will sum all arguments passed to it.
+Quantity of the arguments is unknown.
+
+Use console.log() at the end of the sum() function to print result.
+Use ES6 whenever possible.
+*/
+
+"use strict";
+function sum(...params){
+
+}
+
+sum(1,23,45,6);
+
+
+
+
+
+
+
+
+
+
